8ec84c94736cde5d8591d66b407f4f44da6c596b
yunliang <804408086@qq.com>
2024-05-06 20:25:10 +0800

    fixed

--- docs/index.md
+++ docs/index.md
@@ -12,7 +12,7 @@ nav:
 </div>
 <p style="display:flex;justify-content:space-between;width:220px">
   <a href="https://npmmirror.com/package/lyr-extra">
-    <img alt="npm" src="https://img.shields.io/npm/dw/lyr-extra">
+    <img alt="npm" src="https://img.shields.io/npm/dt/lyr-extra.svg">
   </a>
   <a href="https://npmmirror.com/package/lyr-extra">
     <img alt="NPM downloads" src="https://img.shields.io/npm/v/lyr-extra.svg">
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import * as _BigNumber from 'bignumber.js';
-import html2canvas from 'html2canvas';
-import { Message } from '@arco-design/web-react';
+import * as _BigNumber from "bignumber.js";
+import html2canvas from "html2canvas";
+import { Message } from "@arco-design/web-react";
 
-const ReactToPrint = require('react-to-print')
+const ReactToPrint = require("react-to-print");
 
 const BigNumberjs: any = _BigNumber;
 
 const calculate: any = (
   args: any[],
-  type: 'plus' | 'minus' | 'multipliedBy' | 'dividedBy',
+  type: "plus" | "minus" | "multipliedBy" | "dividedBy"
 ) => {
   return Number(
     args
       .reduce((a, b) => {
         return new BigNumberjs(a)[type](new BigNumberjs(b));
       })
-      .toString(),
+      .toString()
   );
 };
 
@@ -24,7 +24,7 @@ export const encode = (str: string): string => {
     return btoa(encodeURIComponent(str));
   } catch (error) {
     console.log(error);
-    return '';
+    return "";
   }
 };
 
@@ -33,20 +33,20 @@ export const decode = (str: string): string => {
     return decodeURIComponent(atob(str));
   } catch (error) {
     console.log(error);
-    return '';
+    return "";
   }
 };
 
 /** 浮点数运算 */
 export const BigNumber = {
   /** 加 */
-  add: (...args: any[]) => calculate(args, 'plus'),
+  add: (...args: any[]) => calculate(args, "plus"),
   /** 减 */
-  minus: (...args: any[]) => calculate(args, 'minus'),
+  minus: (...args: any[]) => calculate(args, "minus"),
   /** 乘 */
-  multiplie: (...args: any[]) => calculate(args, 'multipliedBy'),
+  multiplie: (...args: any[]) => calculate(args, "multipliedBy"),
   /** 除 */
-  divided: (...args: any[]) => calculate(args, 'dividedBy'),
+  divided: (...args: any[]) => calculate(args, "dividedBy"),
 };
 
 /** 判断空 */
@@ -57,10 +57,10 @@ export const isEmpty = (param: any) => {
   if (Array.isArray(param)) {
     return param.length === 0;
   }
-  if (typeof param === 'string') {
-    return param.trim() === '';
+  if (typeof param === "string") {
+    return param.trim() === "";
   }
-  if (typeof param === 'object') {
+  if (typeof param === "object") {
     return Object.keys(param).length === 0;
   }
   return false;
@@ -69,25 +69,28 @@ export const isEmpty = (param: any) => {
  * 简易uuid
  */
 export const uuid = (size: number) => {
-  return Math.random().toString().substr(2, size);
+  return Math.random()
+    .toString(16)
+    .substring(2, size + 2);
 };
+
 /** 千分位，小数点2位 */
 export const NumberFormat = (
   number: any,
   options = {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  },
+  }
 ) => {
   if (isNaN(Number.parseFloat(number))) {
-    return '-';
+    return "-";
   }
-  return Number(number).toLocaleString('zh-CH', options);
+  return Number(number).toLocaleString("zh-CH", options);
 };
 
 /** 解析url参数 */
-export const getUrlSearchParams = (search = '') => {
-  search = search?.split('?')[1];
+export const getUrlSearchParams = (search = "") => {
+  search = search?.split("?")[1];
   const params: any = {};
   const searchParams: any = new URLSearchParams(search);
   searchParams.forEach((value: any, key: string) => {
@@ -99,11 +102,11 @@ export const getUrlSearchParams = (search = '') => {
 export const downloadFile = (url: string, fileName: string) => {
   return new Promise((res) => {
     const x = new XMLHttpRequest();
-    x.open('GET', url, true);
-    x.responseType = 'blob';
+    x.open("GET", url, true);
+    x.responseType = "blob";
     x.onload = () => {
       const loadurl = window.URL.createObjectURL(x.response);
-      const a = document.createElement('a');
+      const a = document.createElement("a");
       a.href = loadurl;
       a.download = fileName;
       a.click();
@@ -117,77 +120,79 @@ export const copyToClipBoard = async (text: string) => {
   /** navigator clipboard 需要https等安全上下文 */
   if (navigator.clipboard && window.isSecureContext) {
     await navigator.clipboard.writeText(text);
-    Message.success('已复制到剪切板');
+    Message.success("已复制到剪切板");
   } else {
-    const textArea = document.createElement('textarea');
+    const textArea = document.createElement("textarea");
     textArea.value = text;
-    textArea.style.position = 'fixed'; // 解决跳动问题
-    textArea.style.opacity = '0';
-    textArea.style.display = '-999999px';
-    textArea.style.top = '-999999px';
+    textArea.style.position = "fixed"; // 解决跳动问题
+    textArea.style.opacity = "0";
+    textArea.style.display = "-999999px";
+    textArea.style.top = "-999999px";
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
     const res = await new Promise((resolve, reject) => {
-      document.execCommand('copy') ? resolve(true) : reject();
+      document.execCommand("copy") ? resolve(true) : reject();
       textArea.remove();
     });
     if (res) {
-      Message.success('已复制到剪切板');
+      Message.success("已复制到剪切板");
     }
   }
 };
 
 /** 获取元素快照 */
-export const getElementSnapshot = (element: string): {
+export const getElementSnapshot = (
+  element: string
+): {
   printImg: any;
   downloadImg: any;
   getDataURL: any;
 } => {
   return {
     printImg: ReactToPrint.useReactToPrint({
-      bodyClass: 'print-class',
+      bodyClass: "print-class",
       content: () => document.querySelector(element),
     }),
     // 直接下载
     downloadImg: (filename: string) =>
       new Promise((res) => {
-        html2canvas(document.querySelector(element) as any, { useCORS: true }).then(
-          (canvas) => {
-            document.documentElement.classList.remove('html2canvas');
-            const a = document.createElement('a');
-            a.download = filename;
-            a.href = canvas.toDataURL();
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-            res(true);
-          },
-        );
+        html2canvas(document.querySelector(element) as any, {
+          useCORS: true,
+        }).then((canvas) => {
+          document.documentElement.classList.remove("html2canvas");
+          const a = document.createElement("a");
+          a.download = filename;
+          a.href = canvas.toDataURL();
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          res(true);
+        });
       }),
     getDataURL: async () =>
       new Promise((res) => {
-        html2canvas(document.querySelector(element) as any, { useCORS: true }).then(
-          (canvas) => {
-            res(canvas.toDataURL());
-          },
-        );
+        html2canvas(document.querySelector(element) as any, {
+          useCORS: true,
+        }).then((canvas) => {
+          res(canvas.toDataURL());
+        });
       }),
   };
 };
 
-export { default as CheckAppVersion } from './check-app-version';
+export { default as CheckAppVersion } from "./check-app-version";
 
-export { default as ConsoleRender } from './console-render';
+export { default as ConsoleRender } from "./console-render";
 
-export { default as usePureRequest } from './hooks/usePureRequest';
+export { default as usePureRequest } from "./hooks/usePureRequest";
 
-export { default as docxReplace } from './docx-replace';
+export { default as docxReplace } from "./docx-replace";
 
-export { default as docxReplaceBatch } from './docx-replace';
+export { default as docxReplaceBatch } from "./docx-replace";
 
-export { default as babelParse } from './babel-parse';
+export { default as babelParse } from "./babel-parse";
 
-export { default as babelParseCode } from './babel-parse/parse';
+export { default as babelParseCode } from "./babel-parse/parse";
 
-export { default as MarkdownViewer } from './markdown-viewer';
+export { default as MarkdownViewer } from "./markdown-viewer";

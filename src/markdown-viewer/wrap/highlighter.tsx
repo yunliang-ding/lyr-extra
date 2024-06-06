0db35feb7e50c7ac713f98845e5c87f0c8d72384
yunliang <804408086@qq.com>
2024-06-06 19:40:31 +0800

    clean

--- src/markdown-viewer/wrap/highlighter.tsx
+++ src/markdown-viewer/wrap/highlighter.tsx
@@ -4,22 +4,30 @@ import SyntaxLighter from "../syntax-lighter";
 export default ({ tabs, codeTheme, code, source }) => {
   return (
     <div className="markdown-viewer-code-wrap-footer">
-      <Tabs size="mini">
-        {tabs.map((tab, index) => {
-          return (
-            <Tabs.TabPane key={tab} title={tab} style={{ padding: 0 }}>
-              <SyntaxLighter
-                codeTheme={codeTheme}
-                code={String(index === 0 ? code : source[tab]).replace(
-                  /\n$/,
-                  ""
-                )}
-                language={"tsx"}
-              />
-            </Tabs.TabPane>
-          );
-        })}
-      </Tabs>
+      {tabs.length > 1 ? (
+        <Tabs size="mini">
+          {tabs.map((tab: any, index: number) => {
+            return (
+              <Tabs.TabPane key={tab} title={tab} style={{ padding: 0 }}>
+                <SyntaxLighter
+                  codeTheme={codeTheme}
+                  code={String(index === 0 ? code : source[tab]).replace(
+                    /\n$/,
+                    ""
+                  )}
+                  language={"tsx"}
+                />
+              </Tabs.TabPane>
+            );
+          })}
+        </Tabs>
+      ) : (
+        <SyntaxLighter
+          codeTheme={codeTheme}
+          code={String(code).replace(/\n$/, "")}
+          language={"tsx"}
+        />
+      )}
     </div>
   );
 };
--- src/markdown-viewer/wrap/monaco-editor.tsx
+++ src/markdown-viewer/wrap/monaco-editor.tsx
@@ -14,55 +14,62 @@ export default ({
 }) => {
   return (
     <div className="markdown-viewer-code-wrap-footer">
-      <Tabs
-        size="mini"
-        extra={[
-          <Button
-            size="mini"
-            title="点击运行"
-            type="text"
-            style={{
-              marginRight: 4,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: 4,
-            }}
-            onClick={() => {
-              setReload(Math.random());
-            }}
-          >
-            运行
-          </Button>,
-        ]}
+      <Button
+        title="点击运行"
+        type="primary"
+        style={{
+          position: "absolute",
+          bottom: 10,
+          right: 10,
+          zIndex: 9,
+        }}
+        onClick={() => {
+          setReload(Math.random());
+        }}
       >
-        {tabs.map((tab, index) => {
-          return (
-            <Tabs.TabPane key={tab} title={tab} style={{ padding: 0 }}>
-              <CodeEditor
-                require={require}
-                value={String(index === 0 ? code : source[tab]).replace(
-                  /\n$/,
-                  ""
-                )}
-                onChange={(value: string) => {
-                  if (index === 0) {
-                    innerCode.code = value;
-                  } else {
-                    updateRequire[tab] = babelParse({
-                      code: value,
-                      require,
-                    });
-                  }
-                }}
-                style={{ height: 200 }}
-                theme={codeTheme === "dark" ? "vs-dark" : "vs"}
-                mode="function"
-              />
-            </Tabs.TabPane>
-          );
-        })}
-      </Tabs>
+        点击运行
+      </Button>
+      {tabs.length > 1 ? (
+        <Tabs size="mini">
+          {tabs.map((tab: any, index: number) => {
+            return (
+              <Tabs.TabPane key={tab} title={tab} style={{ padding: 0 }}>
+                <CodeEditor
+                  require={require}
+                  value={String(index === 0 ? code : source[tab]).replace(
+                    /\n$/,
+                    ""
+                  )}
+                  onChange={(value: string) => {
+                    if (index === 0) {
+                      innerCode.code = value;
+                    } else {
+                      updateRequire[tab] = babelParse({
+                        code: value,
+                        require,
+                      });
+                    }
+                  }}
+                  style={{ height: 260 }}
+                  theme={codeTheme === "dark" ? "vs-dark" : "vs"}
+                  mode="function"
+                />
+              </Tabs.TabPane>
+            );
+          })}
+        </Tabs>
+      ) : (
+        <CodeEditor
+          require={require}
+          value={String(code).replace(/\n$/, "")}
+          onChange={(value: string) => {
+            innerCode.code = value;
+          }}
+          style={{ height: 260 }}
+          theme={codeTheme === "dark" ? "vs-dark" : "vs"}
+          mode="function"
+        />
+      )}
     </div>
   );
 };

bc58e92fea427fe18112b1c84451e205fa0c8eae
yunliang <804408086@qq.com>
2024-06-06 17:16:21 +0800

    clean

--- src/markdown-viewer/component-wrap.tsx
+++ src/markdown-viewer/component-wrap.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import { Tabs, Tooltip } from "@arco-design/web-react";
+import { Button, Tabs } from "@arco-design/web-react";
 import { CodeEditor } from "lyr-code-editor";
 import { babelParse } from "..";
 
@@ -58,25 +58,28 @@ export default ({
           <Tabs
             size="mini"
             extra={[
-              <Tooltip content="点击运行" position="left">
-                <svg
-                  viewBox="0 0 1024 1024"
-                  width="18"
-                  height="18"
-                  style={{
-                    marginRight: 10,
-                    cursor: "pointer",
-                  }}
-                  onClick={() => {
-                    setReload(Math.random());
-                  }}
-                >
+              <Button
+                size="mini"
+                title="点击运行"
+                type="text"
+                style={{
+                  marginRight: 4,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: 4,
+                }}
+                onClick={() => {
+                  setReload(Math.random());
+                }}
+              >
+                <svg viewBox="0 0 1024 1024" width="18" height="18">
                   <path
                     d="M510.976 64.64a446.72 446.72 0 1 1 0 893.504 446.72 446.72 0 0 1 0-893.44z m0 69.568a375.296 375.296 0 0 0-266.688 110.464A376.96 376.96 0 0 0 364.16 858.88a374.016 374.016 0 0 0 146.816 29.632 375.296 375.296 0 0 0 266.688-110.464 375.808 375.808 0 0 0 110.4-266.688 375.296 375.296 0 0 0-110.464-266.688 376.96 376.96 0 0 0-266.624-110.464zM435.584 300.736a34.816 34.816 0 0 1 36.864 4.48l215.04 175.232a34.56 34.56 0 0 1 0 53.888l-215.04 175.168a34.816 34.816 0 0 1-56.768-26.88V332.16c0-13.44 7.744-25.6 19.84-31.36z m49.728 104.704V609.28l125.056-101.888-125.056-101.952z"
                     fill="#8a8a8a"
-                  ></path>
+                  />
                 </svg>
-              </Tooltip>,
+              </Button>,
             ]}
           >
             {tabs.map((tab, index) => {

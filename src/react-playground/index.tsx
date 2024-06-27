b964b6f3e63d7bf968142e9e5e54c7d8e5abea35
yunliang <804408086@qq.com>
2024-06-27 13:16:38 +0800

    fixed

--- src/react-playground/index.tsx
+++ src/react-playground/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useMemo, useRef } from 'react';
 import { ReactNode, useState } from 'react';
 import { Button, ResizeBox, Tabs } from '@arco-design/web-react';
 import { PlayGroundProps } from './type';
-import { IconRefresh, IconDelete } from '@arco-design/web-react/icon';
 import { babelParse, ConsoleRender } from '..';
 import Editor from './editor';
 import './index.less';
@@ -122,7 +121,14 @@ export default ({
                   activeTab === '0' ? (
                     <Button
                       style={{ marginRight: 8 }}
-                      icon={<IconRefresh />}
+                      icon={
+                        <svg viewBox="0 0 1024 1024" width="16" height="16">
+                          <path
+                            d="M750.933333 273.066667C691.2 209.066667 605.866667 170.666667 512 170.666667c-187.733333 0-341.333333 153.6-341.333333 341.333333s153.6 341.333333 341.333333 341.333333c157.866667 0 290.133333-110.933333 328.533333-256l-89.6 0c-34.133333 98.133333-128 170.666667-238.933333 170.666667-140.8 0-256-115.2-256-256s115.2-256 256-256c72.533333 0 132.266667 29.866667 179.2 76.8L554.666667 469.333333l298.666667 0L853.333333 170.666667 750.933333 273.066667z"
+                            fill="#8a8a8a"
+                          ></path>
+                        </svg>
+                      }
                       onClick={() => {
                         setReload(Math.random());
                       }}
@@ -131,7 +137,18 @@ export default ({
                   ) : (
                     <Button
                       style={{ marginRight: 8 }}
-                      icon={<IconDelete />}
+                      icon={
+                        <svg
+                          viewBox="0 0 1024 1024"
+                          width="16"
+                          height="16"
+                        >
+                          <path
+                            d="M899.1 869.6l-53-305.6H864c14.4 0 26-11.6 26-26V346c0-14.4-11.6-26-26-26H618V138c0-14.4-11.6-26-26-26H432c-14.4 0-26 11.6-26 26v182H160c-14.4 0-26 11.6-26 26v192c0 14.4 11.6 26 26 26h17.9l-53 305.6c-0.3 1.5-0.4 3-0.4 4.4 0 14.4 11.6 26 26 26h723c1.5 0 3-0.1 4.4-0.4 14.2-2.4 23.7-15.9 21.2-30zM204 390h272V182h72v208h272v104H204V390z m468 440V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H416V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H202.8l45.1-260H776l45.1 260H672z"
+                            fill="#8a8a8a"
+                          ></path>
+                        </svg>
+                      }
                       onClick={clear}
                       size="small"
                     />
